c47d860d69d4a6fa78e767541201eb8844f94351
Suleman <suleman.badar.butt@gmail.com>
2026-06-04 17:17:36 +0500

    Resolved the auto login issue

--- src/features/auth/authType.ts
+++ src/features/auth/authType.ts
@@ -39,11 +39,13 @@ export interface ResendOtpRequest {
 
 export interface VerifyOtpResponse {
 	message?: string;
-	user: AuthUser;
-	accessToken: string;
-	expiresAt: number;
-	reportSetting: AuthReportSetting | null;
-	verified: boolean;
+	data: {
+		user: AuthUser;
+		accessToken: string;
+		expiresAt: number;
+		reportSetting: AuthReportSetting | null;
+		verified: boolean;
+	};
 }
 
 export interface ForgotPasswordRequest {
